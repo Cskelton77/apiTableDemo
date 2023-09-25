02583098ba539cff676cb570e721376cfba38859
Cain <Cskelton77@users.noreply.github.com>
2023-09-25 10:54:45 +0100

    Partials dir created (#9)
    Co-authored-by: Cain Skelton <cain@Cains-MacBook-Air.local>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,11 @@
-import Footer from "./Footer/Footer";
-import Header from "./Header/Header";
-import Menu from "./Menu/Menu";
-import PageWrapper from "./PageWrapper/PageWrapper";
+import Footer from "./partials/Footer/Footer";
+import Header from "./partials/Header/Header";
+import Menu from "./partials/Menu/Menu";
+import PageWrapper from "./partials/PageWrapper/PageWrapper";
 import SearchBar from "./SearchBar/SearchBar";
 import Table from "./Table/Table";
 
-import { MenuItem } from './Menu/Menu.interface'
+import { MenuItem } from './partials/Menu/Menu.interface'
 import { TableData } from './Table/Table.interface'
 
 export {
